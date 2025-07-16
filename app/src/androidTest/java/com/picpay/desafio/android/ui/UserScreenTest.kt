216b6077c32907bd7f5dbf2cfe336ab5ff29e161
otavio <17763111+ovcg@users.noreply.github.com>
2025-07-16 12:31:18 -0300

    adiciona testes para MainActivityTest.kt

--- app/src/androidTest/java/com/picpay/desafio/android/ui/UserScreenTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/ui/UserScreenTest.kt
@@ -1,11 +1,10 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.ui
 
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithText
-import com.picpay.desafio.android.ui.UserScreen
 import com.picpay.desafio.android.ui.components.USER_ITEM_TEST_TAG
 import com.picpay.desafio.android.ui.model.UserState
 import com.picpay.desafio.android.ui.theme.UserAppTheme
@@ -15,7 +14,8 @@ import org.junit.Test
 
 class UserScreenTest {
 
-    @get:Rule val composeTestRule = createComposeRule()
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
     @Test
     fun showUserScreen() {
