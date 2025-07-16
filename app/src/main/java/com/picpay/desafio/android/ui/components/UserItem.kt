5a7e0e2190162c45565ed67fe7cc458b923d73a4
otavio <17763111+ovcg@users.noreply.github.com>
2025-07-16 12:43:00 -0300

    altera tag test

--- app/src/main/java/com/picpay/desafio/android/ui/components/UserItem.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/components/UserItem.kt
@@ -59,7 +59,7 @@ fun UserItem(user: User) {
                 modifier =
                     Modifier.padding(start = 16.dp, top = 24.dp)
                         .wrapContentSize()
-                        .testTag(USER_ITEM_NAME_TEST_TAG),
+                        .testTag(USER_ITEM_USERNAME_TEST_TAG),
                 text = user.username ?: "",
                 color = Color.White,
                 style = Typography.labelMedium,
@@ -67,7 +67,7 @@ fun UserItem(user: User) {
             Text(
                 modifier =
                     Modifier.padding(start = 16.dp, bottom = 8.dp)
-                        .testTag(USER_ITEM_USERNAME_TEST_TAG),
+                        .testTag(USER_ITEM_NAME_TEST_TAG),
                 text = user.name ?: "",
                 color = ColorDetail,
                 style = Typography.labelMedium,
